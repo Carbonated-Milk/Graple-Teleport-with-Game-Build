65c7853601640b125680b71cb3a03d1a65a86f63
Carbonated-Milk <98993346+Carbonated-Milk@users.noreply.github.com>
2022-03-21 20:32:35 -0700

    ne

--- Graple Teleport Game/Graple Teleport Game/Assets/Doubler.cs
+++ Graple Teleport Game/Graple Teleport Game/Assets/Doubler.cs
@@ -7,8 +7,6 @@ public class Doubler : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        Debug.Log("clone");
         if (other.GetComponent<Rigidbody2D>())
         {
             GameObject clone = Instantiate(other.gameObject);
